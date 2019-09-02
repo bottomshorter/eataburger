bfcd0f0524de2b75905cda7debb05de3fdc4eb57
bottomshorter <lamvanthony@gmail.com>
2019-09-01 17:51:31 -0700

    work?

--- config/connection.js
+++ config/connection.js
@@ -3,19 +3,19 @@ const mysql = require("mysql");
 require('dotenv').config({path: '../.env'});
 let connection = null;
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+// if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection('mysql://hbgq0pip4u1d0y2i:js7vpsg21t5g5ep8@sulnwdk5uwjw1r2k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ukg9x211cldfyj0n');
   console.log("👍🏼You have conneted via JAWSDB..👍🏼")
-} else {
-  console.log("👍🏼You have conneted via localhost..👍🏼");
-  connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "example123!",
-    database: "burgers_db"
-  });
-}
+// } else {
+//   console.log("👍🏼You have conneted via localhost..👍🏼");
+//   connection = mysql.createConnection({
+//     port: 3306,
+//     host: "localhost",
+//     user: "root",
+//     password: "example123!",
+//     database: "burgers_db"
+//   });
+// }
 
 
 // Make connection.
