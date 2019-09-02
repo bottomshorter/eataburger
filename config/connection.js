e9f52831eada177412cfbfe3257bb317cdf2735c
bottomshorter <lamvanthony@gmail.com>
2019-09-01 17:56:31 -0700

    JawsDB Functional

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ require('dotenv').config({path: '../.env'});
 let connection = null;
 
 // if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection('mysql://hbgq0pip4u1d0y2i:js7vpsg21t5g5ep8@sulnwdk5uwjw1r2k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ukg9x211cldfyj0n');
+  connection = mysql.createConnection('mysql://hbgq0pip4u1d0y2i:y74xt8unymm53xkb@sulnwdk5uwjw1r2k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ukg9x211cldfyj0n');
   console.log("👍🏼You have conneted via JAWSDB..👍🏼")
 // } else {
 //   console.log("👍🏼You have conneted via localhost..👍🏼");
